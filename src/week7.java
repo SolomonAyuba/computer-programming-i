d3a0c9cdded7da0f2d26c02f08f018ba2f399e7c
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-06-27 15:03:44 +0100

    Upload

--- src/week7.java
+++ src/week7.java
@@ -11,7 +11,7 @@ public class week7 {
         Defining a Class
         A class is a template for creating objects with shared attributes and behaviours. Attributes are also known as fields or properties.
         Behaviours are implemented as methods.
-        Example - validating commit
+        Example - validating commit reupload++
          */
 
         class Student {
